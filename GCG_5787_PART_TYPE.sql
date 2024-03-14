59608a92921053022b06d5156d551ed38b418f37
Service Web <sw@gssmail.com>
2024-03-14 16:47:01 -0500

    Commit changes in C:\Websites\Git\emarlow\5787\GCG_5787_PART_TYPE.sql

---
+++ GCG_5787_PART_TYPE.sql
@@ -0,0 +1,12 @@
+-- Database: GLOBALCIG Metadata version: 2 Exported: Mar 9, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_5787_PART_TYPE" USING 'GCG_5787_PART_TYPE.mkd' PAGESIZE=4096 (
+ "CLASS_ID" CHAR(6),
+ "TYPE_ID" CHAR(6),
+ "PART_PREFIX" CHAR(6),
+ "PART_TEMPLATE_DESC" CHAR(30) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
